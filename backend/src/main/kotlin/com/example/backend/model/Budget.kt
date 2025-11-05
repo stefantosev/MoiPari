923400068f9c2bddcecac5a5703bdb80dbff7171
Todor Mitevski <74872391+todormitevski@users.noreply.github.com>
2025-11-05 18:50:34 +0100

    hotfix: change category controller, fix serialization loops (#4)

--- backend/src/main/kotlin/com/example/backend/model/Budget.kt
+++ backend/src/main/kotlin/com/example/backend/model/Budget.kt
@@ -1,23 +1,23 @@
 package com.example.backend.model
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.ObjectIdGenerators
 import jakarta.persistence.*
 
-
 @Entity
-@Table(name="budget")
-data class Budget (
+@Table(name = "budget")
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
+data class Budget(
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
-    val id: Int=0,
-
-    val monthlyLimit: Float=0F,
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int = 0,
+    val monthlyLimit: Float = 0F,
 
     @Column(name = "budget_month")
-    val month: Int=0,
+    val month: Int = 0,
 
     @Column(name = "budget_year")
-    val year: Int=0,
-
+    val year: Int = 0,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
--- backend/src/main/kotlin/com/example/backend/repository/CategoryRepository.kt
+++ backend/src/main/kotlin/com/example/backend/repository/CategoryRepository.kt
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CategoryRepository : JpaRepository<Category, Int> {
-}
+interface CategoryRepository : JpaRepository<Category, Int> {}
