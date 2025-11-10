d865ff41607c9846489ce407358859aac5bbf315
Todor Mitevski <todormitevski5@gmail.com>
2025-11-10 19:24:13 +0100

    feat: add auth context state, welcome page on first launch

--- mobile/lib/pages/welcome.dart
+++ mobile/lib/pages/welcome.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mobile/navigation_provider.dart';
-
+import 'package:mobile/providers/auth_provider.dart';
 
 class WelcomePage extends ConsumerWidget {
   const WelcomePage({super.key});
@@ -15,59 +13,53 @@ class WelcomePage extends ConsumerWidget {
         height: double.infinity,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-              colors: [Color(0xFFFFDEE9), Color(0xFFFCC2FF)],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-          )
+            colors: [Color(0xFFFFDEE9), Color(0xFFFCC2FF)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
         ),
-        child: Stack(
-          children: [
-            Center(
-              child: SingleChildScrollView(
-                child: Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset("assets/images/MoiPari.png"),
-                        SizedBox(height: 30),
-                        Text(
-                          "Manage your money and set limits",
-                           style: TextStyle(
-                             fontSize: 20,
-                             color: Colors.black,
-                             fontWeight: FontWeight.bold
-                           ),
-                        ),
-                        SizedBox(height: 60),
-                        SizedBox(
-                          child: ElevatedButton(
-                              onPressed: () {
-                                ref.read(navigationIndexProvider.notifier).state = 0;
-                              },
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.deepPurpleAccent,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10)
-                                ),
-                              ),
-                              child: Text(
-                                "Get started",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold
-                                ),
-                              )
-                          ),
-                        )
-                      ],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/MoiPari.png"),
+                  SizedBox(height: 30),
+                  Text(
+                    "Manage your money and set limits",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
-                ),
+                  ),
+                  SizedBox(height: 60),
+                  ElevatedButton(
+                    onPressed: () {
+                      ref.read(authStateProvider.notifier).completeWelcome();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurpleAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "Get started",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
